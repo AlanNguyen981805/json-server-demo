bfe72db374287a3d452217e2490ee3a31afe175b
Nguyễn Hiệp <hiepnn@MacBook-Pro-cua-Nguyen.local>
2021-09-16 16:27:27 +0700

    update

--- main.js
+++ main.js
@@ -23,7 +23,8 @@ const middlewares = jsonServer.defaults()
 // })
 
 // Use default router
-server.use(router)
-server.listen(3000, () => {
+server.use('/api', router);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
   console.log('JSON Server is running')
 })
